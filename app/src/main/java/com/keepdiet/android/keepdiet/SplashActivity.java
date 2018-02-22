b583b492c709e85209ff4bea26bff99ebb0c8a59
pieworkplace <pieworkplace@gmail.com>
2018-02-22 01:42:03 -0500

    Completed splash screen.

--- app/src/main/java/com/keepdiet/android/keepdiet/SplashActivity.java
+++ app/src/main/java/com/keepdiet/android/keepdiet/SplashActivity.java
@@ -1,13 +1,23 @@
 package com.keepdiet.android.keepdiet;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
+    private static final int MILLISECOND_TO_WAIT = 3000;
+    Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, MILLISECOND_TO_WAIT);
     }
 }

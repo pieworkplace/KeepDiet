91644b1c397727e83aa8e9b6b624f9b9a143a255
pieworkplace <pieworkplace@gmail.com>
2018-03-01 20:52:43 -0500

    Add bottom navigation bar and their icons.

--- app/src/main/java/com/keepdiet/android/keepdiet/MainActivity.java
+++ app/src/main/java/com/keepdiet/android/keepdiet/MainActivity.java
@@ -1,7 +1,10 @@
 package com.keepdiet.android.keepdiet;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.keepdiet.android.keepdiet.utils.BottomNavigationViewHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //forbid bottom navigation view shifting mode
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_bar);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
     }
 }

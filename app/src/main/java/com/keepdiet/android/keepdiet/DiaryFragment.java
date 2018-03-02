2287c3bf1b0c872d315215e150b8d51b24bcbd1c
pieworkplace <pieworkplace@gmail.com>
2018-03-02 17:18:17 -0500

    Added the target bar for diary. Start dealing with viewpager

--- app/src/main/java/com/keepdiet/android/keepdiet/DiaryFragment.java
+++ app/src/main/java/com/keepdiet/android/keepdiet/DiaryFragment.java
@@ -2,10 +2,13 @@ package com.keepdiet.android.keepdiet;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -18,7 +21,6 @@ public class DiaryFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -26,4 +28,33 @@ public class DiaryFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_diary, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setData(view);
+    }
+
+    private void setData(View view) {
+        int target = 2000;
+        ((TextView) view.findViewById(R.id.diary_target_number)).setText(String.format("%,d",target));
+        int food = 800;
+        ((TextView) view.findViewById(R.id.diary_food_number)).setText(String.format("%,d", food));
+        int exercise = 500;
+        ((TextView) view.findViewById(R.id.diary_exercise_number)).setText(String.format("%,d", exercise));
+        int remain = target - food + exercise;
+        ((TextView) view.findViewById(R.id.diary_remain_number)).setText(String.format("%,d", remain));
+    }
+
+    private class DiaryPagerAdapter extends PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return false;
+        }
+    }
 }

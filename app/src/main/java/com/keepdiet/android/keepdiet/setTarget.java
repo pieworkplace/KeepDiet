2f058b4303d39683adcb994e7d3e347ee2d3b070
Haoting <hli617@gatech.edu>
2018-03-04 00:20:02 -0500

    added start group

---
+++ app/src/main/java/com/keepdiet/android/keepdiet/setTarget.java
@@ -0,0 +1,13 @@
+package com.keepdiet.android.keepdiet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class setTarget extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_target);
+    }
+}
---
+++ app/src/main/java/com/keepdiet/android/keepdiet/startGroup.java
@@ -0,0 +1,13 @@
+package com.keepdiet.android.keepdiet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class startGroup extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_group);
+    }
+}
